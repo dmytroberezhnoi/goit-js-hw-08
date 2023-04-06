9c0d10e54d901bc031596285d86fb4666e4e85f7
Dmytro Berezhnoi <berezhnoi.dmytro@gmail.com>
2023-04-06 13:46:44 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+const onPlay = throttle(function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}, 1000);
+
+player.on('timeupdate', onPlay);
+
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+player
+  .setCurrentTime(currentTime || 0)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
